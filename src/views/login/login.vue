669791f0fcb0cb24f5d57ba12088f4ea60355bb4
瞿强鑫 <quqiangxin@ut.cn>
2021-06-03 18:03:00 +0800

    登陆页面初始化

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>Login</div>
+  <div class="container">
+    <div class="login-box">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="form.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password" @keyup.enter.native="handleLogin"></el-input>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="handleLogin">登录</el-button>
+          <el-button>注册</el-button>
+        </div>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,11 +23,53 @@ export default {
   name: 'Login',
   data() {
     return {
-
+      form: {
+        userName: null,
+        password: null
+      },
+      rules: {
+        userName: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 11 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 11 个字符', trigger: 'blur' }
+        ],
+      }
     }
   },
   methods: {
-
+    handleLogin() {
+      if (this.form.userName === 'admin' && this.form.password === 'admin') {
+        this.$router.push({
+          name: 'Home'
+        })
+      } else {
+        this.$message.error('用户名或密码不正确！')
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+.login-box {
+  margin-top: 300px;
+  padding: 20px 20px 0;
+  width: 700px;
+  height: 200px;
+  background-color: lightblue;
+}
+.login-btn {
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+}
+</style>
